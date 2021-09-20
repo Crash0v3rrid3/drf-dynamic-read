053ff7add6f5a04a266c867212aa315c384072ac
Z3r0c0o1 <jain240298@gmail.com>
2021-09-20 22:14:05 +0530

    Updated the package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="drf-dynamic-read",
-    version="0.1.0",
+    version="0.1.1",
     description="",
     url="https://github.com/Crash0v3rrid3/drf-dynamic-read",
     author="",
